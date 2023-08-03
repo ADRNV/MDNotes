43cea339f7491a9553784fd977e9bb9d7fe54423
NKT <49834917+ADRNV@users.noreply.github.com>
2023-08-03 19:43:21 +0300

    Add .NET Solution

---
+++ MdNotesServer/MdNotes.WebApi/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace MdNotes.WebApi
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;   
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("MD Notes API", new OpenApiInfo 
+                { 
+                    Title = "MD NOtes API",
+                    Version = "V1",
+                    Description = "API for MD Notes"
+                });
+
+                var xmlFile = Assembly.GetExecutingAssembly()
+                .GetName()
+                .Name + ".xml";
+
+                var path = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(path);
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.DocumentTitle = "MD Notes API";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(c => c.MapControllers());
+        }
+    }
+}
