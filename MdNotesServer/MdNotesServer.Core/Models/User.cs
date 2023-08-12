6602d7de7e367da8d95f24b2045ea9639fc98ce3
ADRN_NKT <combinerk@gmail.com>
2023-08-12 23:33:43 +0300

    Add Core models

---
+++ MdNotesServer/MdNotesServer.Core/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MdNotesServer.Core.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public List<Note> Notes {get; set;}
+    }
+}
