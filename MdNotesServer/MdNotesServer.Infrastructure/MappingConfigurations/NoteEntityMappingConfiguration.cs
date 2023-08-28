b7ab1b1a58914d656d76716874774742b8cf30e5
ADRN_NKT <combinerk@gmail.com>
2023-08-29 00:38:05 +0300

    Add map

---
+++ MdNotesServer/MdNotesServer.Infrastructure/MappingConfigurations/NoteEntityMappingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace MdNotesServer.Infrastructure.MappingConfigurations
+{
+    public class NoteEntityMappingConfiguration : Profile
+    {
+        public NoteEntityMappingConfiguration()
+        {
+            CreateMap<NoteEntity, NoteCore>()
+                .ReverseMap()
+                .ForMember(n => n.User, o => o.Ignore());
+        }
+    }
+}
